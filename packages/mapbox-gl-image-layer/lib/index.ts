5844b14414e8359e55e0159d47463eb00dd04454
huanglii <naivemap@163.com>
2023-05-05 22:43:00 +0800

    feature: mask

--- packages/mapbox-gl-image-layer/lib/index.ts
+++ packages/mapbox-gl-image-layer/lib/index.ts
@@ -2,13 +2,12 @@ import { satisfies } from 'compare-versions'
 import earcut from 'earcut'
 import mapboxgl from 'mapbox-gl'
 import * as twgl from 'twgl.js'
+
 import fs from './shaders/image.fragment.glsl'
 import vs from './shaders/image.vertex.glsl'
 import maskfs from './shaders/mask.fragment.glsl'
 import maskvs from './shaders/mask.vertex.glsl'
 
-import { loadImage } from './utils/image'
-import { createProgram } from './utils/webgl'
 import { initArrugator } from './utils/arrugator'
 import type { ArrugadoFlat, Coordinates } from './utils/arrugator'
 
@@ -90,7 +89,7 @@ export default class ImageLayer implements mapboxgl.CustomLayerInterface {
     // 主程序
     this.programInfo = twgl.createProgramInfo(gl, [vs, fs])
 
-    this._loadImage(map, gl)
+    this.loadTexture(map, gl)
     this.bufferInfo = twgl.createBufferInfoFromArrays(gl, {
       a_pos: { numComponents: 2, data: this.arrugado.pos },
       a_uv: { numComponents: 2, data: this.arrugado.uv },
@@ -102,42 +101,8 @@ export default class ImageLayer implements mapboxgl.CustomLayerInterface {
       const { data } = this.maskProperty
       if (data) {
         this.maskProgramInfo = twgl.createProgramInfo(gl, [maskvs, maskfs])
-        let positions: number[] = []
-        let triangles: number[] = []
-        if (data.type === 'MultiPolygon') {
-          // type: 'MultiPolygon'
-          const polyCount = data.coordinates.length
-          let triangleStartIndex = 0
-          for (let i = 0; i < polyCount; i++) {
-            const coordinates = data.coordinates[i]
-            const flatten = earcut.flatten(coordinates)
-            const { vertices, holes, dimensions } = flatten
-            const triangle = earcut(vertices, holes, dimensions)
-            const triangleNew = triangle.map((item) => item + triangleStartIndex)
 
-            triangleStartIndex += vertices.length / 2
-            // positions.push(...vertices)
-            // triangles.push(...triangleNew)
-            for (let m = 0; m < vertices.length; m++) {
-              positions.push(vertices[m])
-            }
-            for (let n = 0; n < triangleNew.length; n++) {
-              triangles.push(triangleNew[n])
-            }
-          }
-        } else {
-          // type: 'Polygon'
-          const flatten = earcut.flatten(data.coordinates)
-          const { vertices, holes, dimensions } = flatten
-          positions = vertices
-          triangles = earcut(vertices, holes, dimensions)
-        }
-
-        this.maskBufferInfo = twgl.createBufferInfoFromArrays(gl, {
-          a_pos: { numComponents: 2, data: positions },
-          indices:
-            triangles.length / 3 > 65535 ? new Uint32Array(triangles) : new Uint16Array(triangles),
-        })
+        this.maskBufferInfo = this.getMaskBufferInfo(gl, data)
       }
     }
   }
@@ -220,89 +185,71 @@ export default class ImageLayer implements mapboxgl.CustomLayerInterface {
       gl.clear(gl.STENCIL_BUFFER_BIT)
       gl.disable(gl.STENCIL_TEST)
     }
-    // if (this._loaded && this._program) {
-    //   gl.useProgram(this._program)
-
-    //   // matrix
-    //   gl.uniformMatrix4fv(gl.getUniformLocation(this._program, 'u_matrix'), false, matrix)
-    //   // pos
-    //   gl.bindBuffer(gl.ARRAY_BUFFER, this._positionBuffer)
-    //   gl.vertexAttribPointer(gl.getAttribLocation(this._program, 'a_pos'), 2, gl.FLOAT, false, 0, 0)
-    //   // uv
-    //   gl.bindBuffer(gl.ARRAY_BUFFER, this._uvBuffer)
-    //   gl.vertexAttribPointer(gl.getAttribLocation(this._program, 'a_uv'), 2, gl.FLOAT, false, 0, 0)
-
-    //   // index
-    //   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this._verticesIndexBuffer)
-
-    //   // texture
-    //   gl.activeTexture(gl.TEXTURE0)
-    //   gl.bindTexture(gl.TEXTURE_2D, this._texture)
-    //   gl.uniform1i(gl.getUniformLocation(this._program, 'u_sampler'), 0)
-
-    //   // opacity
-    //   gl.uniform1f(gl.getUniformLocation(this._program, 'u_opacity'), this._option.opacity || 1)
-
-    //   // blend
-    //   gl.enable(gl.BLEND)
-    //   gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
-
-    //   gl.drawElements(gl.TRIANGLES, this._arrugado.trigs.length, gl.UNSIGNED_SHORT, 0)
-
-    //   gl.bindBuffer(gl.ARRAY_BUFFER, null)
-    //   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null)
-    // }
   }
 
   /**
-   * Updates the image URL and, optionally, the projection, the coordinates and the resampling.
+   * Updates the URL, the projection, the coordinates, the opacity or the resampling of the image.
    * @param {Object} option Options object.
-   * @param {string} [option.url] Required image URL.
+   * @param {string} [option.url] Image URL.
    * @param {string} [option.projection] Projection with EPSG code that points to the image..
    * @param {Array<Array<number>>} [option.coordinates] Four geographical coordinates,
+   * @param {number} [option.opacity] opacity of the image.
    * @param {string} [option.resampling] The resampling/interpolation method to use for overscaling.
    */
   updateImage(option: {
-    url: string
+    url?: string
     projection?: string
     coordinates?: Coordinates
+    opacity?: number
     resampling?: 'linear' | 'nearest'
   }) {
-    // this._loaded = false
-    // this._option.url = option.url
-    // if (this._gl && this._map) {
-    //   if (option.projection || option.coordinates) {
-    //     this._option.projection = option.projection ?? this._option.projection
-    //     this._option.coordinates = option.coordinates ?? this._option.coordinates
-    //     // reinit arrugator
-    //     this._arrugado = initArrugator(this._option.projection, this._option.coordinates)
-    //     this._bindData(this._gl, this._arrugado)
-    //   }
-    //   this._option.resampling = option.resampling ?? this._option.resampling
-    //   // reload image
-    //   this._loadImage(this._map, this._gl)
-    // }
-    // return this
+    if (this.gl && this.map) {
+      this.option.opacity = option.opacity ?? this.option.opacity
+      if (option.projection || option.coordinates) {
+        this.option.projection = option.projection ?? this.option.projection
+        this.option.coordinates = option.coordinates ?? this.option.coordinates
+        // reinit arrugator
+        this.arrugado = initArrugator(this.option.projection, this.option.coordinates)
+        this.bufferInfo = twgl.createBufferInfoFromArrays(this.gl, {
+          a_pos: { numComponents: 2, data: this.arrugado.pos },
+          a_uv: { numComponents: 2, data: this.arrugado.uv },
+          indices: this.arrugado.trigs,
+        })
+      }
+      if (option.url || option.resampling) {
+        this.loaded = false
+        this.option.url = option.url ?? this.option.url
+        this.option.resampling = option.resampling ?? this.option.resampling
+        // reload image
+        this.loadTexture(this.map, this.gl)
+      } else {
+        this.map.triggerRepaint()
+      }
+    }
+    return this
   }
 
-  // private _bindData(gl: WebGLRenderingContext, arrugado: ArrugadoFlat) {
-  //   gl.bindBuffer(gl.ARRAY_BUFFER, this._positionBuffer)
-  //   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(arrugado.pos), gl.STATIC_DRAW)
+  /**
+   * Updates the mask property
+   * @param {MaskProperty} mask The mask property.
+   */
+  updateMask(mask: Partial<MaskProperty>) {
+    if (this.gl && this.map && this.maskProgramInfo) {
+      this.maskProperty = Object.assign(this.maskProperty, mask)
+      this.maskBufferInfo = this.getMaskBufferInfo(this.gl, this.maskProperty.data)
+      this.map.triggerRepaint()
+    }
+  }
 
-  //   gl.bindBuffer(gl.ARRAY_BUFFER, this._uvBuffer)
-  //   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(arrugado.uv), gl.STATIC_DRAW)
-
-  //   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this._verticesIndexBuffer)
-  //   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(arrugado.trigs), gl.STATIC_DRAW)
-  // }
-
-  private _loadImage(map: mapboxgl.Map, gl: WebGLRenderingContext) {
+  private loadTexture(map: mapboxgl.Map, gl: WebGLRenderingContext) {
     // 创建纹理
     const filter = this.option.resampling === 'nearest' ? gl.NEAREST : gl.LINEAR
+
     this.texture = twgl.createTexture(
       gl,
       {
         src: this.option.url,
+        crossOrigin: this.option.crossOrigin,
         minMag: filter,
         flipY: 0,
       },
@@ -311,16 +258,47 @@ export default class ImageLayer implements mapboxgl.CustomLayerInterface {
         map.triggerRepaint()
       }
     )
-    loadImage(this.option.url, this.option.crossOrigin).then((img) => {
-      // 创建纹理
-      // this.texture = gl.createTexture()
-      // gl.bindTexture(gl.TEXTURE_2D, this.texture)
-      // const textureFilter = this.option.resampling === 'nearest' ? gl.NEAREST : gl.LINEAR
-      // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, textureFilter)
-      // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, textureFilter)
-      // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
-      // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
-      // gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img)
+  }
+
+  private getMaskBufferInfo(
+    gl: WebGLRenderingContext,
+    data: GeoJSON.Polygon | GeoJSON.MultiPolygon
+  ) {
+    let positions: number[] = []
+    let triangles: number[] = []
+    if (data.type === 'MultiPolygon') {
+      // type: 'MultiPolygon'
+      const polyCount = data.coordinates.length
+      let triangleStartIndex = 0
+      for (let i = 0; i < polyCount; i++) {
+        const coordinates = data.coordinates[i]
+        const flatten = earcut.flatten(coordinates)
+        const { vertices, holes, dimensions } = flatten
+        const triangle = earcut(vertices, holes, dimensions)
+        const triangleNew = triangle.map((item) => item + triangleStartIndex)
+
+        triangleStartIndex += vertices.length / 2
+        // positions.push(...vertices)
+        // triangles.push(...triangleNew)
+        for (let m = 0; m < vertices.length; m++) {
+          positions.push(vertices[m])
+        }
+        for (let n = 0; n < triangleNew.length; n++) {
+          triangles.push(triangleNew[n])
+        }
+      }
+    } else {
+      // type: 'Polygon'
+      const flatten = earcut.flatten(data.coordinates)
+      const { vertices, holes, dimensions } = flatten
+      positions = vertices
+      triangles = earcut(vertices, holes, dimensions)
+    }
+
+    return twgl.createBufferInfoFromArrays(gl, {
+      a_pos: { numComponents: 2, data: positions },
+      indices:
+        triangles.length / 3 > 65535 ? new Uint32Array(triangles) : new Uint16Array(triangles),
     })
   }
 }
